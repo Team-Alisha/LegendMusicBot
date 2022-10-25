52cd97cf2038737c4f89538cfbee1f8bc396d5b5
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-10-25 15:41:06 +0530

    💫

---
+++ LegendMusic/__init__.py
@@ -0,0 +1,41 @@
+from LegendMusic.core.bot import LegendBot
+from LegendMusic.core.dir import dirr
+from LegendMusic.core.git import git
+from LegendMusic.core.userbot import Userbot
+from LegendMusic.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = LegendBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
